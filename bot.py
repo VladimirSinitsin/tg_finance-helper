c0f9eb8bd413806bc92bf2e5504e10e3abde5f49
VladimirSinitsin <vladimir.sinitsin.ya@yandex.ru>
2020-09-16 15:19:30 +0300

    Небольшие правки по форматированию

--- bot.py
+++ bot.py
@@ -8,31 +8,33 @@ bot = telebot.TeleBot(TOKEN)
 expenses = {}
 
 
-@bot.message_handler(commands = ['start'])
+@bot.message_handler(commands=['start'])
 def send_welcom(message):
-	bot.send_message(message.chat.id, 'Приве я бот для ведения бюджета.\n' +
-									  'Ты можешь записывать сюда свои расходы \n ' +
-									  'а их буду подсчитывать. Пример бананы - 300\n\n' +
-									  'Доступные команды \n/start\n/help\n/expenses')
+	bot.send_message(message.chat.id, 'Привет я бот для ведения бюджета.\n' +
+									  'Ты можешь записывать сюда свои расходы, \n ' +
+									  'а их буду подсчитывать. Пример: бананы - 300\n\n' +
+									  'Доступные команды: \n/start\n/help\n/expenses')
 
-@bot.message_handler(commands = ['help'])
+
+@bot.message_handler(commands=['help'])
 def send_help(message):
-	bot.send_message(message.chat.id,'К сожалению на данный момент вам никто не поможет)))')
+	bot.send_message(message.chat.id, 'К сожалению на данный момент вам никто не поможет)))')
 
-@bot.message_handler(commands = ['expenses'])
+
+@bot.message_handler(commands=['expenses'])
 def send_help(message):
 	global expenses
 	if expenses:
-		answer =''
+		answer = ''
 		for key, value in expenses.items():
 			answer += '{0}: {1}\n', format(key, value)
 
-		bot.send_message(message.chat.id,'Все затраты:\n'+answer)
+		bot.send_message(message.chat.id, 'Все затраты:\n'+answer)
 	else:
-		bot.send_message(message.chat.id,'Вы не вводили затраты')
+		bot.send_message(message.chat.id, 'Вы не вводили затраты')
 
 
-@bot.message_handler(content_typs = ["text"])
+@bot.message_handler(content_typs=["text"])
 def echo_expenses(message):
 	global expenses
 	text = message.text.split()
@@ -42,7 +44,7 @@ def echo_expenses(message):
 		expenses[name] += value
 	else:
 		expenses[name] = value
-	bot.send_message(message.chat.id, 'Добавленны затраты \n{0} на сумму {1}',format(name, expenses[name]))
+	bot.send_message(message.chat.id, 'Добавленны затраты \n{0} на сумму {1}', format(name, expenses[name]))
 
 
 bot.polling()
