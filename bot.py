2bf85ea3e248bf5d38ecfaa67f18121b7b592ade
VladimirSinitsin <vladimir.sinitsin.ya@yandex.ru>
2020-09-16 14:40:23 +0300

    Базовая версия бота

--- bot.py
+++ bot.py
@@ -1,60 +1,44 @@
-"""
-Основной тестовый файл телеграм бота: t.me/CuteBudgetBot
-"""
 import telebot
 
-from bot_config import TOKEN
+bot = telebot.TeleBot("1348964593:AAEIbkuC4E7cra88p11xjtyy3FucW-scOj0")
+
+expenses = {}
+
+@bot.massage_handler(commands = ['start'])
+def send_welcom(massage):
+	bot.send_massage(massage.chat.id, 'Приве я бот для ведения бюджета.\n' +
+									  'Ты можешь записывать сюда свои расходы \n ' +
+									  'а их буду подсчитывать. Пример бананы - 300\n\n' +
+									  'Доступные команды \n/start\n/help\n/expenses')
+
+@bot.massage_handler(commands = ['help'])
+def send_help(massage):
+	bot.send_massage(massage.chat.id,'К сожалению на данный момент вам никто не поможет)))')
+
+@bot.massage_handler(commands = ['expenses'])
+def send_help(massage):
+	global expenses
+	if expenses:
+		answer =''
+		for key, value in expenses.items():
+			answer += '{0}: {1}\n', format(key, value)
+
+		bot.send_massage(massage.chat.id,'Все затраты:\n'+answer)
+	else:
+		bot.send_massage(massage.chat.id,'Вы не вводили затраты')
 
 
-# Инициализация бота.
-bot = telebot.TeleBot(TOKEN, parse_mode=None)
-
-# Словарь для хранения продуктов и их стоимостей.
-products = {}
-
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, 'Привет! Я бот для ведения бюджета\n''' +
-                          'Вводи свои покупки в виде: название - цена\n' +
-                          'Например: бананы - 300\n\n'
-                          'Доступные команды:\n/start\n/help\n/all')
-
-
-@bot.message_handler(commands=['help'])
-def send_help(message):
-    bot.reply_to(message, 'Если тебе нужна помощь, то обратись к разработчикам')
-
-
-# ВЫвод всех покупок и сумм через /all.
-@bot.message_handler(commands=['all'])
-def preview_all_products(message):
-    global products
-    if products:
-        result = ''
-        # Записываем все товары и их суммы в строку для вывода.
-        for key, value in products.items():
-            result += '{0}: {1}\n'.format(key, value)
-        bot.reply_to(message, 'Всё покупки:\n\n' + result)
-    else:
-        bot.reply_to(message, 'Покупок не было')
-
-
-# Добавление покупки.
-@bot.message_handler(content_types=["text"])
-def echo_product(message):
-    global products
-    # Разбиваем строку, где 1 элемент - название товара, а последний - его стоимость.
-    text = message.text.split()
-    name_of_product = text[0]
-    value = int(text[-1])
-    # Если уже был, то просто суммируем, иначе создаем новый товар в словаре.
-    if name_of_product in products:
-        products[name_of_product] += value
-        value = products[name_of_product]
-    else:
-        products[name_of_product] = value
-    bot.reply_to(message, 'Добавлен товар: {0}\nна сумму: {1}'.format(name_of_product, value))
+@bot.massage_handler(content_typs = ["text"])
+def echo_expenses(massage):
+	global expenses
+	text = massage.text.split()
+	name = text[0]
+	value = int(text[-1])
+	if name in expenses:
+		expenses[name] += value
+	else:
+		expenses[name] = value
+	bot.send_massage(massage.chat.id, 'Добавленны затраты \n{0} на сумму {1}',format(name, expenses[name]))
 
 
 bot.polling()
